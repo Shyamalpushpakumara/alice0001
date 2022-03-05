6472126c6ded854e024f1fcacb2e2539a1c1ea62
ArisuXD <96303829+ArisuXD@users.noreply.github.com>
2022-03-05 07:45:28 +0530

    Update subreddit.js

--- plugins/subreddit.js
+++ plugins/subreddit.js
@@ -1,14 +1,14 @@
-let fetch = require('node-fetch')
+let fetch = require("node-fetch")
 
-let handler = async (m, { conn, text, isOwner }) => {
-  let res = await fetch(API('https://meme-api.herokuapp.com', '/gimme/' + encodeURI(text || ''), {}))
-  if (!res.ok) throw eror
+let handler = async (m, { conn, text }) => {
+  let res = await fetch(global.API('https://meme-api.herokuapp.com', '/gimme/' + encodeURI(text || ''), {}))
+  if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.url) throw 'Media Not Found!'
-  if (json.nsfw && !db.data.settings[conn.user.jid].nsfw) return conn.sendButton(m.chat, 'NSFW!', '© Alice 🥀', isOwner ? 'Turn ON', isOwner ? '.1 nsfw')
-  await conn.sendFile(m.chat, json.url, text, json.title, m)
+  if (!json.url) throw 'Media not found!'
+  if (json.nsfw) throw 'Content blocked'
+  await conn.sendFile(m.chat, json.url, text, json.title, m, false, { thumbnail: Buffer.alloc(0) })
 }
-handler.help = ['subreddit <search>']
+handler.help = ['subreddit/sr <query>']
 handler.tags = ['internet']
 handler.command = /^(sr|subreddit)$/i
 
