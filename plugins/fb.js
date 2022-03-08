cc1f47b55fc40082185a21a37e66d7b159a97f85
ArisuXD <96303829+ArisuXD@users.noreply.github.com>
2022-03-08 07:48:55 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     // m.reply(require('util').format(json))
     if (!json.status) throw json
     await m.reply(wait)
-    await conn.sendFile(m.chat, json.data[0].url, '', '© stikerin', m)
+    await conn.sendFile(m.chat, json.data[0].url, '', '© Alice 🥀', m)
   }).catch(_ => _)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
