0ad5f9e3ac2099de3548e6df20ba98e3ff316865
ArisuXD <96303829+ArisuXD@users.noreply.github.com>
2022-03-31 18:42:25 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,19 +1,19 @@
-const { facebook } = require('../lib/facebook')
+const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. wheres the url?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-  if (!args[0].match(/https:\/\/.*(facebook.com|fb.watch)/gi)) throw `url salah`
-  facebook(args[0]).then(async res => {
-    let fb = JSON.stringify(res)
-    let json = JSON.parse(fb)
-    // m.reply(require('util').format(json))
-    if (!json.status) throw json
-    await m.reply(wait)
-    await conn.sendFile(m.chat, json.data[0].url, '', '© Alice 🥀', m)
-  }).catch(_ => _)
+  if (!args[0]) throw `uhm.. where is the url?\n\nexample:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
+  if (/^https?:\/\/.*(m.facebook.com||www.facebook.com|facebook.com)/i.test(m.text)) throw `only support url from FB lite and web only _please check url again_`
+
+  let res = await fetch(API('xteam', '/dl/fbv2', { url: args[0] }, 'APIKEY'))
+  if (!res.ok) return m.reply(eror)
+  let json = await res.json()
+  await m.reply(wait)
+  await conn.sendFile(m.chat, json.result.hd.url, '', '© Alice 🥀', m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+
+handler.premium = false
 
 module.exports = handler
